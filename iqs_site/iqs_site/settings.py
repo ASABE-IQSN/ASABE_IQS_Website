5766ea05aca7ecec0eaa85c36104386bdb69fce0
dking1115 <davidking1115@me.com>
2025-12-04 20:46:13 -0600

    updating static

--- iqs_site/iqs_site/settings.py
+++ iqs_site/iqs_site/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["internationalquarterscale.com","127.0.0.1"]
 
-
+STATICFILES_DIRS = ["/var/www/quarterscale/static"]
 # Application definition
 
 INSTALLED_APPS = [
